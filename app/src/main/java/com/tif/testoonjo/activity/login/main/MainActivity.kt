a92516c1daf7d1c5d50fa97f81e796e0d7e3a106
Envy- <wisnuandrian325@gmail.com>
2020-10-06 18:43:24 +0700

    clear - activity login .

--- app/src/main/java/com/tif/testoonjo/activity/login/main/MainActivity.kt
+++ app/src/main/java/com/tif/testoonjo/activity/login/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tif.testoonjo
+package com.tif.testoonjo.activity.login.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tif.testoonjo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
